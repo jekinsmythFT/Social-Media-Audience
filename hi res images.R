bb3b196784782e2fdd92b7bd26fc41c005f71d93
Ekinsmyth <jack.ekinsmyth@ft.com>
2021-03-10 17:43:40 +0000

    10 march

--- hi res images.R
+++ hi res images.R
@@ -118,3 +118,143 @@ ds %>%
 
 
 ggsave("subssocial.png", plot = last_plot(), dpi = 400, width = 11, height = 7)
+
+
+
+
+ds %>% 
+  select(content_associate) %>% 
+  drop_na() %>% 
+  mutate(person = row_number(),
+         content_associate = str_split(content_associate, ",")) %>% 
+  unnest(cols = c(content_associate)) %>% 
+  group_by(content_associate) %>% 
+  summarise(n = n()) %>%
+  arrange(-n) %>% 
+  mutate(prop = n / 654, 
+         content_associate = factor(content_associate),
+         is_high = if_else(prop >= .5, 1, 0)) %>% 
+  ggplot(aes(x = reorder(content_associate, + prop), y = prop)) + 
+  geom_segment(aes(x =reorder(content_associate, + prop), xend = reorder(content_associate, + prop), yend = prop, y = 0 )) +
+  geom_point(size = 6, aes(col = factor(is_high)), show.legend = F) +
+  scale_color_manual(values = c("#F0E442", "#009E73")) +
+  geom_text(aes(label = percent(prop, 1)), vjust = 2) +
+  coord_flip() +
+  scale_y_continuous(labels = percent_format()) +
+  scale_x_discrete(expand = c(.1,.1)) +
+  labs(title = "\nSocial media followers most associate us with\nFinance & Markets",
+       subtitle = "\nWhich of the following content do you associate with the FT? (Select any that apply)\n",
+       y = "\nPercentage of Followers",
+       x = "Content Areas") +
+  theme_minimal()+
+  theme(
+    axis.title = element_text(size = 16),
+    axis.text = element_text(size = 16),
+    legend.text = element_text(size = 10),
+    legend.title = element_text(size = 12),
+    plot.title = element_text(size = 24),
+    plot.subtitle = element_text(size = 16),
+    legend.position = "bottom") 
+
+ggsave("contentassoc.png", plot = last_plot(), dpi = 400, width = 11, height = 7)
+
+
+ds %>% 
+  select(social_follow, content_associate) %>% 
+  mutate(person = row_number(),
+         social_follow = str_split(social_follow, ","),
+         content_associate = str_split(content_associate, ",")) %>% 
+  unnest(cols = social_follow) %>% 
+  unnest(cols = content_associate) %>% 
+  group_by(content_associate, social_follow) %>% 
+  summarise(n = n()) %>% 
+  drop_na() %>% 
+  ungroup() %>% 
+  left_join(social_media_follows, by = c("social_follow")) %>% 
+  mutate(prop = n.x / n.y,
+         alpha = if_else(content_associate %in% c("Finance & Markets",
+                                                  "Companies",
+                                                  "World news",
+                                                  "Politics"), 1, 0),
+         label = if_else(social_follow == "YouTube", 
+                         paste0(content_associate, '\n', percent(prop, 1)),
+                         percent(prop, 1))) %>% 
+  filter(social_follow %in% c("Facebook", "Instagram", "LinkedIn", "YouTube")) %>% 
+  {  ggplot(., aes(social_follow, prop, col = reorder(content_associate, + prop),
+                   alpha = alpha)) +
+      geom_text_repel(data = subset(., alpha ==1), aes(label = label, col = content_associate),
+                      show.legend = F) +
+      coord_flip() +
+      geom_point(show.legend = F) +
+      guides(alpha = F) +
+      scale_y_continuous(labels = percent_format()) +
+      labs(y = "\nPercentage of followers",
+           x = "Social Media Platforms",
+           subtitle = "\nSocial media platforms by what content followers associate the FT with\n",
+           title = "\nThere is only marginal differences in which content followers\nfrom different social media platforms associate us with")+
+      theme_minimal()+
+      theme(
+        axis.title = element_text(size = 16),
+        axis.text = element_text(size = 16),
+        legend.text = element_text(size = 10),
+        legend.title = element_text(size = 12),
+        plot.title = element_text(size = 24),
+        plot.subtitle = element_text(size = 16),
+        legend.position = "bottom") 
+  }
+
+ggsave("platcontentassoc1.png", plot = last_plot(), dpi = 400, width = 11, height = 7)
+
+
+
+ds %>% 
+  select(social_follow, content_associate) %>% 
+  mutate(person = row_number(),
+         social_follow = str_split(social_follow, ","),
+         content_associate = str_split(content_associate, ",")) %>% 
+  unnest(cols = social_follow) %>% 
+  unnest(cols = content_associate) %>% 
+  group_by(content_associate, social_follow) %>% 
+  summarise(n = n()) %>% 
+  drop_na() %>% 
+  ungroup() %>% 
+  left_join(social_media_follows, by = c("social_follow")) %>% 
+  mutate(prop = n.x / n.y,
+         alpha = if_else(content_associate %in% c("Finance & Markets",
+                                                  "Companies",
+                                                  "World news",
+                                                  "Politics"), 0, 1),
+         label = if_else(social_follow == "YouTube", 
+                         paste0(content_associate, '\n', percent(prop, 1)),
+                         percent(prop, 1))) %>% 
+  filter(social_follow %in% c("Facebook", "Instagram", "LinkedIn", "YouTube")) %>% 
+  {  ggplot(., aes(social_follow, prop, col = reorder(content_associate, + prop),
+                   alpha = alpha)) +
+      geom_text_repel(data = subset(., alpha ==1), aes(label = label, col = content_associate),
+                      show.legend = F) +
+      coord_flip() +
+      geom_point(show.legend = F) +
+      guides(alpha = F) +
+      scale_y_continuous(labels = percent_format()) +
+      labs(y = "\nPercentage of followers",
+           x = "Social Media Platforms",
+           subtitle = "\nFacebook followers are less likely to associate us with Tech & Climate, but more likely to\nassoicate us with Life & Arts\n",
+           title = "\nFacebooks folowers are the most distinct")+
+      theme_minimal()+
+      theme(
+        axis.title = element_text(size = 16),
+        axis.text = element_text(size = 16),
+        legend.text = element_text(size = 10),
+        legend.title = element_text(size = 12),
+        plot.title = element_text(size = 24),
+        plot.subtitle = element_text(size = 16),
+        legend.position = "bottom") 
+  }
+
+ggsave("platcontentassoc2.png", plot = last_plot(), dpi = 400, width = 11, height = 7)
+
+
+
+
+
+
